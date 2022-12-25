6acff75771f186deda21151452284c1e22149890
Rodrigo Vaz <rodrigodp2014@gmail.com>
2022-12-25 16:49:31 -0300

    feat: init unit tests

---
+++ tests/Goodread.Core.Test/Builders/AuthorBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Bogus;
+using Goodreads.Core.Entities;
+using Goodreads.Core.Enums;
+
+namespace Goodread.Core.Test.Builders;
+
+public class AuthorBuilder
+{
+    static readonly Faker faker = new();
+
+    static Random _random = new();
+    private static int _ramdomNumber = _random.Next(110);
+    
+    private string Name = faker.Person.FullName;
+    private string City = faker.Address.City();
+    private string State = faker.Address.StateAbbr();
+    private string Country = faker.Address.Country();
+    private DateTime Birthdate = faker.Date.Past(_ramdomNumber);
+    private string Website = faker.Internet.Url();
+    private string Description = faker.Random.Words();
+    public Gender Gender = faker.PickRandom<Gender>();
+
+    public static AuthorBuilder New() => new();
+    
+    public AuthorBuilder AuthorWithName(string name)
+    {
+        Name = name;
+        return this;
+    }
+    
+    public AuthorBuilder AuthorWithCity(string city)
+    {
+        City = city;
+        return this;
+    }
+    
+    public AuthorBuilder AuthorWithState(string state)
+    {
+        State = state;
+        return this;
+    }
+    
+    public AuthorBuilder AuthorWithCountry(string country)
+    {
+        Country = country;
+        return this;
+    }
+    
+    public AuthorBuilder AuthorWithBirthdate(DateTime birthdate)
+    {
+        Birthdate = birthdate;
+        return this;
+    }
+    
+    public AuthorBuilder AuthorWithGender(Gender gender)
+    {
+        Gender = gender;
+        return this;
+    }
+
+    public Author Build() =>
+        new(Name, City, State, Country, Birthdate, Website, Description, Gender);
+}
---
+++ tests/Goodread.Core.Test/Entities/AuthorTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Goodread.Core.Test.Builders;
+using Xunit;
+
+namespace Goodreads.Core.Test.Entities;
+
+public class AuthorTest
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [Trait("Domain", "Author")]
+    public void Should_Not_Add_Author_With_Invalid_Name(string invalidName)
+    {
+        var author = AuthorBuilder.New().AuthorWithName(invalidName).Build();
+
+        author.Valid.Should().BeFalse();
+        author.ValidationResult.Errors.Should().NotBeEmpty();
+        author.ValidationResult.Errors.Should().HaveCount(c => c > 0).And.OnlyHaveUniqueItems();
+        author.ValidationResult.Errors.Should().Contain(x => x.ErrorMessage.Contains("Name"));
+    }
+}
